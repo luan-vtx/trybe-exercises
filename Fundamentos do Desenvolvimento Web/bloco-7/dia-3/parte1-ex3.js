7692137b473c3fdb55fd5581634b27a3e868a183
Luan Xavier <luantenorio3@gmail.com>
2021-04-29 12:16:09 -0300

    Parte 1 - Exercicio 3

---
+++ Fundamentos do Desenvolvimento Web/bloco-7/dia-3/parte1-ex3.js
@@ -0,0 +1,27 @@
+const assert = require('assert');
+
+function myRemoveWithoutCopy(arr, item) {
+  for (let index = 0, len = arr.length; index < len; index += 1) {
+    if (arr[index] === item) {
+      arr.splice(index, 1);
+      index -= 1;
+      len -= 1;
+    }
+  }
+
+  return arr;
+}
+
+// implemente seus testes aqui
+
+assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 4]);
+assert.notDeepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 3, 4]);
+
+const myArray = [2, 4, 6, 8];
+
+myRemoveWithoutCopy(myArray, 4);
+
+assert.deepStrictEqual(myArray, [2, 4, 6, 8], 'Erro: os arrays não são iguais.');
+
+assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 5), [1, 2, 3, 4]);
+
