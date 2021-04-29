405ab03922c61741ca6d19435623a7bdd12cf439
Luan Xavier <luantenorio3@gmail.com>
2021-04-29 12:28:58 -0300

    Parte 1 - Exercicio 4

---
+++ Fundamentos do Desenvolvimento Web/bloco-7/dia-3/parte1-ex4.js
@@ -0,0 +1,15 @@
+const assert = require('assert');
+
+function myFizzBuzz(num) {
+  if (typeof num !== 'number') return false;
+  if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
+  if (num % 3 === 0) return 'fizz';
+  if (num % 5 === 0) return 'buzz';
+  return num;
+}
+
+// implemente seus testes aqui
+assert.deepStrictEqual(myFizzBuzz(15), 'fizzbuzz');
+assert.deepStrictEqual(myFizzBuzz(6), 'fizz');
+assert.deepStrictEqual(myFizzBuzz(10), 'buzz');
+assert.deepStrictEqual(myFizzBuzz('peixe'), false);
